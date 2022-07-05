4248784204c8c81c0077e983a54a0bf941b770a7
Sami Sharif <96096653+sshar1@users.noreply.github.com>
2022-07-05 13:41:45 -0400

    Update cam.py

--- Vision Projects/Face Alarm/cam.py
+++ Vision Projects/Face Alarm/cam.py
@@ -1,1 +1,26 @@
+import cv2 as cv
+from cvzone.FaceDetectionModule import FaceDetector
+from cvzone.SerialModule import SerialObject
 
+cap = cv.VideoCapture(0)
+
+detector = FaceDetector()
+arduino = SerialObject()
+
+while cap.isOpened():
+    ret, img = cap.read()
+
+    if not ret:
+        break
+
+    img, bboxes = detector.findFaces(img)
+
+    if bboxes:
+        arduino.sendData([1])
+    else:
+        arduino.sendData([0])
+
+    cv.imshow("Image", img)
+    cv.waitKey(1)
+
+arduino.sendData([0])
